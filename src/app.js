a8d7a31ebffaf5e1c15b2d25e0c226462b376559
KevinSDJ <sebadjkevin@gmail.com>
2022-03-10 15:47:39 -0300

    .

--- src/app.js
+++ src/app.js
@@ -26,10 +26,10 @@ const {NODE_ENV}=process.env
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use(cors({credentials: true, origin:"*"}))
+server.use(cors({credentials: true, origin:'*'}))
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin: *'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin','*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE,HEAD');
