13a080789d747848a086d62f52eea33d0f3aceca
KevinSDJ <sebadjkevin@gmail.com>
2022-03-10 17:27:59 -0300

    .

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin','*');
   res.header('Access-Control-Allow-Credentials',true);
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Authorization,content-type,application/json');
+  res.header("Access-Control-Allow-Headers", 'Content-Type,Authorization,content-type,application/json');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
