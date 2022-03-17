c0d4b36534960439529b563d988f09b57891028b
KevinSDJ <sebadjkevin@gmail.com>
2022-03-17 14:39:50 -0300

    .

--- src/app.js
+++ src/app.js
@@ -42,14 +42,12 @@ server.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://dogs-app-client.vercel.app");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
   next();
 });
 
 
 server.use('/', routes);
-
-
 
 server.use((err, req, res, next) => { 
   const status = err.status || 500;
