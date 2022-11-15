d620fbe3d034ba2cfee9fb4b8050961bceab2a83
kevin De jesus <sebadjkevin@gmail.com>
2022-11-14 23:05:05 -0300

    fix cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const morgan = require('morgan');
 const routes = require('./routes/index.js');
 require('dotenv').config()
 
-let domain=process.env.NODE_ENV==='production'&&'https://dogs-app-client.vercel.app'||'http://localhost:3000'
 
 const server = express();
 server.name = 'API';
@@ -13,7 +12,7 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin',domain); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin','*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE,HEAD');
